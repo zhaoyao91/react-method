c4d6b6741171b050d6f6769eefa2ee5ec9ac81e7
zhaoyao91 <zhaoyao91@163.com>
2017-06-06 14:19:06 +0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export function createMethod () {
+  let _method = null
+
+  return {
+    invoke(...args) {
+      return _method(...args)
+    },
+
+    define(method) {
+      _method = method
+    },
+  }
+}
+
+export function withMethod (name) {
+  return function (Component) {
+    return class WithMethod extends Component {
+      _method = createMethod()
+
+      render () {
+        return <Component {...this.props} {...{[name]: this._method}}/>
+      }
+    }
+  }
+}
+
+/**
+ * @param name
+ * @param definition # (props) => (...args)
+ * @returns {function} HOC
+ */
+export function defineMethod (name, definition) {
+  return function (Component) {
+    return class DefineMethod extends Component {
+      componentWillMount () {
+        this.defineMethod(this.props)
+      }
+
+      componentWillReceiveProps (nextProps) {
+        this.defineMethod(nextProps)
+      }
+
+      render () {
+        return <Component {...this.props}/>
+      }
+
+      defineMethod (props) {
+        const define = this.props[name]
+        define(definition(props))
+      }
+    }
+  }
+}
